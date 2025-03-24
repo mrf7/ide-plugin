a410c1b2d24d28e3673ad59a8d4f9ed3d323ae82
Michael Friend <Michael.friend2@accesscfa.com>
2025-03-24 10:53:45 -0400

    take scope

--- src/main/kotlin/com/github/mrf7/ideplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mrf7/ideplugin/services/MyProjectService.kt
@@ -4,13 +4,20 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.github.mrf7.ideplugin.MyBundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Service(Service.Level.PROJECT)
-class MyProjectService(project: Project) {
+class MyProjectService(project: Project, val scope: CoroutineScope) {
 
     init {
         thisLogger().info(MyBundle.message("projectService", project.name))
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        scope.launch {
+            delay(1000)
+            println("Done")
+        }
     }
 
     fun getRandomNumber() = (1..100).random()
